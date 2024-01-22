ea66869b31bc76fc20fd2c82ffa23067d8009baf
Nader Chatti <chatti.tkd@gmail.com>
2024-01-22 09:51:17 +0100

    adding linkedin and email

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/naderchatti/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Linkedin
+        </a>
+        <a
+          className="App-link"
+          href="mailto:contact@naderchatti.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Email
         </a>
       </header>
     </div>
