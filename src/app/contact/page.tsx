22480622b6cee1b8d91c5887a4f4ccebbaef6684
chattinader <chatti.tkd@gmail.com>
2024-06-04 20:49:13 +0200

    adding contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+const Contact = () => {
+  return (
+    <div className={styles.main}>
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
