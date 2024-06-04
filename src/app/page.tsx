d312e1cd15340f181f8712132457bc7e010f76bc
chattinader <chatti.tkd@gmail.com>
2024-06-04 20:48:43 +0200

    updating home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Nader CHATTI</h1>
+      <h1>Home</h1>
     </main>
   );
 }
