4f0c2d8227ab0455caec645ed150299fb2f7c94d
chattinader <chatti.tkd@gmail.com>
2024-06-05 21:03:48 +0200

    adding hero section with animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,11 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Home</h1>
+      <section className={styles.hero}>
+        <span className={styles.hello}>Hello, I&apos;m</span>
+        <h1 className={styles.name}>Nader CHATTI</h1>
+        <h4 className={styles.role}>Full Stack Developer</h4>
+      </section>
     </main>
   );
 }
