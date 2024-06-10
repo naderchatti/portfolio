0dd8e0fbfcb925210cda39574e79120c95df4193
chattinader <chatti.tkd@gmail.com>
2024-06-10 02:56:26 +0200

    exporting framework slider to component

---
+++ src/components/Slider/FrameworkSlider.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import styles from './Slider.module.css';
+import { useResponsive } from '@/context/ResponsiveContext';
+import CustomSVGs from '../CustomSVGs/CustomSVGs';
+
+const frameworks = [
+  'NextJS',
+  'React',
+  'NodeJS',
+  'Angular',
+  'Python',
+  'HTML',
+  'JS',
+  'CSS',
+  'Java',
+];
+
+export const FrameworkSlider = () => {
+  const { isMobile } = useResponsive();
+
+  return (
+    <div
+      className={styles.slider}
+      style={
+        {
+          '--item-width': isMobile ? '3.5rem' : '6rem',
+          '--item-height': isMobile ? '2rem' : '3rem',
+          '--quantity': '9',
+        } as React.CSSProperties
+      }
+    >
+      <div className={styles.list}>
+        {frameworks?.map((framework, index) => {
+          return (
+            <div
+              key={index}
+              className={styles.item}
+              style={
+                {
+                  '--item-position': (index + 1).toString(),
+                } as React.CSSProperties
+              }
+            >
+              <CustomSVGs iconName={framework} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
