75450bf58c865a9e5de902445a458d9784392eea
naderchatti <chatti.tkd@gmail.com>
2024-10-06 02:50:30 +0200

    fix logo eye animation issue

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -28,70 +28,7 @@ export default function Home() {
 
   const [visibleItems, setVisibleItems] = useState({});
 
-  const logoRef = useRef<HTMLDivElement>(null);
-
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
   const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) => {
-      setMousePosition({ x: event.clientX, y: event.clientY });
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
-
-  useEffect(() => {
-    const logo = logoRef.current;
-    if (!logo) return;
-
-    const leftEye = logo.querySelector('#leftEye');
-    const rightEye = logo.querySelector('#rightEye');
-    const leftEyeCenter = { x: 200, y: 310 };
-    const rightEyeCenter = { x: 310, y: 310 };
-    const maxMovement = 3;
-
-    function moveEye(
-      eye: SVGElement,
-      eyeCenter: { x: number; y: number },
-      mouseX: number,
-      mouseY: number
-    ) {
-      const dx = mouseX - eyeCenter.x;
-      const dy = mouseY - eyeCenter.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-      const angle = Math.atan2(dy, dx);
-
-      const moveX = Math.min(maxMovement, distance) * Math.cos(angle);
-      const moveY = Math.min(maxMovement, distance) * Math.sin(angle);
-
-      eye.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    }
-
-    const handleMouseMove = (event: MouseEvent) => {
-      const svgRect = logo.getBoundingClientRect();
-      const mouseX = event.clientX - svgRect.left;
-      const mouseY = event.clientY - svgRect.top;
-
-      if (leftEye instanceof SVGElement) {
-        moveEye(leftEye, leftEyeCenter, mouseX, mouseY);
-      }
-      if (rightEye instanceof SVGElement) {
-        moveEye(rightEye, rightEyeCenter, mouseX, mouseY);
-      }
-    };
-
-    document.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -154,7 +91,7 @@ export default function Home() {
             isHeroImageVisible ? styles.animate : ''
           }`}
         >
-          <Logo mouseX={mousePosition.x} mouseY={mousePosition.y} />
+          <Logo />
         </div>
       </div>
       <section className={styles.hero}>
