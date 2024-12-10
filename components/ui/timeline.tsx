c6de299fdb27ec5b1b430ba272309d7ef7bc874e
naderchatti <chatti.tkd@gmail.com>
2024-12-10 03:29:23 +0100

    fix timeline small screens padding

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -51,7 +51,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               </h3>
             </div>
 
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
+            <div className="relative pl-[3.25rem] pr-4 md:pl-4 w-full">
               <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-muted-foreground">
                 {item.title}
               </h3>
