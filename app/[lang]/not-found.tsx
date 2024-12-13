c3e9f3381c5648b1ad33bf54e37bd5a390ec8fc5
naderchatti <chatti.tkd@gmail.com>
2024-12-13 21:47:01 +0100

    adding edge runtime for non-static  routes

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import { SkillsSlider } from '@/components/skills-slider';
 import { Card } from '@/components/ui/card';
 import TextReveal from '@/components/ui/text-reveal';
