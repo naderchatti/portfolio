0d983d9226d284b85b3a32b9dc383a7b4be29a35
naderchatti <chatti.tkd@gmail.com>
2024-12-14 03:57:37 +0100

    fix attempt

--- next.config.mjs
+++ next.config.mjs
@@ -9,20 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Permissions-Policy',
-            value:
-              'private-state-token-redemption=(), private-state-token-issuance=(), browsing-topics=()',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
